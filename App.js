83c807c66213112c200077f8236b34dbc7e76273
Jonathan Watson <thejmdw@gmail.com>
2022-02-22 10:35:12 -0500

    amplify configured, cognito setup

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
 
-export default function App() {
+Amplify.configure(awsconfig)
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -18,3 +23,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App)
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "swipehometest17a3a0bb6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
